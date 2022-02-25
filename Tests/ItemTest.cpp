7ec88efcb755abfe10d25be040e201ec81197207
Zongyuan Li <lizongy1@cse.msu.edu>
2022-02-25 11:38:11 -0500

    Item test added

--- Tests/ItemTest.cpp
+++ Tests/ItemTest.cpp
@@ -9,32 +9,33 @@
 #include <Game.h>
 
 using namespace std;
+const std::wstring DoorImageName = L"images/door.png";
 
 /** Mock class for testing item class */
-//class ItemMock : public Item
-//{
-//public:
-//    ItemMock(Game *game) : Item(game) {}
-//};
+class ItemMock : public Item
+{
+public:
+    ItemMock(Game *game) : Item(game, DoorImageName) {}
+};
 
-//TEST(ItemTest, Construct)
-//{
-//    Game game;
-//    ItemMock item(&game);
-//}
+TEST(ItemTest, Construct)
+{
+    Game game;
+    ItemMock item(&game);
+}
 
-//TEST(ItemTest, GettersSetters)
-//{
-//    Game game;
-//    ItemMock item(&game);
-//
-//    // Tests SetLocation, GetX, GetY
-//    item.SetLocation(100, 100);
-//    ASSERT_NEAR(100, item.GetX(), 0.0001);
-//    ASSERT_NEAR(100, item.GetX(), 0.0001);
-//
-//    // Test a second location
-//    item.SetLocation(233, -79);
-//    ASSERT_NEAR(233, item.GetX(), 0.0001);
-//    ASSERT_NEAR(-79, item.GetY(), 0.0001);
-//}
+TEST(ItemTest, GettersSetters)
+{
+    Game game;
+    ItemMock item(&game);
+
+    // Tests SetLocation, GetX, GetY
+    item.SetLocation(100, 100);
+    ASSERT_NEAR(100, item.GetX(), 0.0001);
+    ASSERT_NEAR(100, item.GetX(), 0.0001);
+
+    // Test a second location
+    item.SetLocation(233, -79);
+    ASSERT_NEAR(233, item.GetX(), 0.0001);
+    ASSERT_NEAR(-79, item.GetY(), 0.0001);
+}
