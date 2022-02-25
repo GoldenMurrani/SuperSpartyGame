3ff5a3b9060c3f12252f7b0f25404020bce5a024
Yousif Murrani <murraniy@msu.edu>
2022-02-25 00:05:12 -0500

    Made a GameTest and ItemTest (had to temporarily comment out part of Item Constructor)

---
+++ Tests/GameTest.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file GameTest.cpp
+ * @author Yousif Murrani
+ */
+
+#include <pch.h>
+#include "gtest/gtest.h"
+#include <Game.h>
+
+using namespace std;
+
+TEST(GameTest, Construct)
+{
+    Game game;
+}
---
+++ Tests/ItemTest.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file ItemTest.cpp
+ * @author Yousif Murrani
+ */
+
+#include <pch.h>
+#include "gtest/gtest.h"
+#include <Item.h>
+#include <Game.h>
+
+using namespace std;
+
+/** Mock class for testing item class */
+class ItemMock : public Item
+{
+public:
+    ItemMock(Game *game) : Item(game) {}
+};
+
+TEST(ItemTest, Construct)
+{
+    Game game;
+    ItemMock item(&game);
+}
+
+TEST(ItemTest, GettersSetters)
+{
+    Game game;
+    ItemMock item(&game);
+
+    // Tests SetLocation, GetX, GetY
+    item.SetLocation(100, 100);
+    ASSERT_NEAR(100, item.GetX(), 0.0001);
+    ASSERT_NEAR(100, item.GetX(), 0.0001);
+
+    // Test a second location
+    item.SetLocation(233, -79);
+    ASSERT_NEAR(233, item.GetX(), 0.0001);
+    ASSERT_NEAR(-79, item.GetY(), 0.0001);
+}
