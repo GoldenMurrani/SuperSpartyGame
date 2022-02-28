ccce23aef9a43a0eb1d27796fc4342e04d5123c8
Yousif Murrani <murraniy@msu.edu>
2022-02-28 16:29:25 -0500

    Added comments to scoreboard

--- Project1Lib/Scoreboard.cpp
+++ Project1Lib/Scoreboard.cpp
@@ -6,6 +6,25 @@
 #include "pch.h"
 #include "Scoreboard.h"
 
+/**
+ * Function to initialize a timer
+ */
+void Scoreboard::SetTime()
+{
+
+}
+
+/**
+ * Function to reset the timer to 0
+ */
+void Scoreboard::ResetTimer()
+{
+
+}
+
+/**
+ * Function to draw scoreboard on screen
+ */
 void Scoreboard::Draw()
 {
 
